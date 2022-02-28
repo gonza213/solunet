ab4fdfc47b90e5d83a75fa85f9b8bd4d9cda1634
Gonzalo <gonzaa.vidal.18@gmail.com>
2022-02-28 00:54:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Diseño web de Solunet
---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// Input range
+document.getElementById("rangeDownload").oninput = function () {
+    this.style.background = 'linear-gradient(to right, #5385b7 0%, #5385b7 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+};
+document.getElementById("rangeCpu").oninput = function () {
+    this.style.background = 'linear-gradient(to right, #5385b7 0%, #5385b7 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+};
+document.getElementById("rangeIps").oninput = function () {
+    this.style.background = 'linear-gradient(to right, #5385b7 0%, #5385b7 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+};
